9ab10b90e2d8170c137c54f70712221003631d2a
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-05 14:30:48 -0500

    Basic query

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors');
 const routes = require('./routes/');
+
+const schema = require('./graphql/schema')
 
 
 const app = express();
 app.use(cors());
-app.use('/', routes);
+// app.use('/', routes);
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  }))
 
 
 module.exports = app;
---
+++ src/graphql/schema.js
@@ -0,0 +1,19 @@
+const graphql = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql
+
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world'
+        },
+      },
+    },
+  }),
+})
+
+module.exports = schema
